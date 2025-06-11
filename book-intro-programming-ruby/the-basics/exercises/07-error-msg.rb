6d952ddda7e44cb5e50a29acd0b1db1d68f11b13
Sebastian Bachmann <github@sebastianbachmann.de>
2025-06-11 15:06:16 +0200

    fix: added comment

--- book-intro-programming-ruby/the-basics/exercises/07-error-msg.rb
+++ book-intro-programming-ruby/the-basics/exercises/07-error-msg.rb
@@ -2,4 +2,4 @@
 #  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
 
 
-There is a missing closing parenthesis in the previous line.
+# There is a missing closing parenthesis in the previous line.
